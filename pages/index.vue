bced190bc30753c94ac916b93db181933ad90a46
Matteo Biasi <matteobiasi@flashbeing.com>
2021-12-20 12:48:43 +0100

    feat: added shops ordering

--- pages/index.vue
+++ pages/index.vue
@@ -313,6 +313,17 @@ export default {
         }
       })
 
+      // Order stores by name
+      unique.sort((a, b) => {
+        if (a.name < b.name) {
+          return -1
+        }
+        if (a.name > b.name) {
+          return 1
+        }
+        return 0
+      })
+
       return {
         all,
         unique,
